097416b8b4ac862c83a3d9d8a8153b72a2709189
M100achuz2 <68661509+M100achuz2@users.noreply.github.com>
2020-12-28 08:27:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# Search bot for articles from the Androtips channel
+# Created by YeudaBy t.me/m100achuz
+# https://github.com/M100achuz2/androtipSearchBot
+
 from pyrogram import Client
 
 if __name__ == '__main__':
